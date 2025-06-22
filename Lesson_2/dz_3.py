b2bd4e6a521697ed42ea90d386ac1d85fd4350d6
code-hub404 <seva8a.k@gmail.com>
2025-06-22 19:07:11 +0300

    dz_3

--- Lesson_2/dz_3.py
+++ Lesson_2/dz_3.py
@@ -9,10 +9,8 @@ left1, right1 = divmod(right, b)
 left2, right2 = divmod(right1, c)
 left3, right3 = divmod(right2, d)
 left4, right4 = divmod(right3, e)
-
 left, left1, left2, left3, left4 = left4, left3, left2, left1, left
 num1 = (left * 10000 + left1 * 1000 + left2 * 100 + left3 * 10 + left4 * 1)
-
 print(num1)
 
 
